ac2acd1b7a6f900c9d955117de86303eac558c57
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-29 19:29:54 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,52 +14,48 @@ f1 = []
 f2 = []
 
 
-
-
 def scrape_data():
+    data = requests.get("http://ufcstats.com/statistics/events/upcoming")
+    soup = BeautifulSoup(data.text, 'html.parser')
+    table = soup.find('table', {"class": "b-statistics__table-events"})
+    links = table.find_all('a', href=True)
 
-        data = requests.get("http://ufcstats.com/statistics/events/upcoming")
+    for link in links:
+        all_links.append(link.get('href'))
+
+    for link in all_links:
+        print(f"Now currently scraping link: {link}")
+
+        data = requests.get(link)
         soup = BeautifulSoup(data.text, 'html.parser')
-        table = soup.find('table', {"class": "b-statistics__table-events"})
-        links = table.find_all('a', href=True)
+        time.sleep(1)
 
-        for link in links:
-            all_links.append(link.get('href'))
+        rows = soup.find_all('tr', {
+            "class": "b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click"})
 
-        for link in all_links:
-            print(f"Now currently scraping link: {link}")
+        for row in rows:
 
-            data = requests.get(link)
-            soup = BeautifulSoup(data.text, 'html.parser')
-            time.sleep(1)
+            h2 = soup.find("h2")
+            e_name.append(h2.text.strip())
 
-            
+            fighters = row.find_all('a', {"href": re.compile("http://ufcstats.com/fighter-details")})
 
-            rows = soup.find_all('tr', {"class": "b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click"})
+            try:
+                f1.append(fighters[0].text.strip())
+                f2.append(fighters[1].text.strip())
+            except IndexError:
+                f1.append("null")
+                f2.append("null")
+            continue
 
-            for row in rows:
-                    
-                    h2 = soup.find("h2")
-                    e_name.append(h2.text.strip())
-
-                    fighters = row.find_all('a', {"href": re.compile("http://ufcstats.com/fighter-details")})
-
-                    try:
-                        f1.append(fighters[0].text.strip())
-                        f2.append(fighters[1].text.strip())
-                    except IndexError:
-                        f1.append("null")
-                        f2.append("null")
-                    continue
+    return None
 
 
-        return None
-      
-      #preprocessing
+# preprocessing
 # remove rows where DOB is null
 # impute stance as orthodox for missing stances
 def create_df():
-    #create empty dataframe
+    # create empty dataframe
     df = pd.DataFrame()
 
     df["Event"] = e_name
@@ -67,30 +63,30 @@ def create_df():
     df["Fighter2"] = f2
 
     return df
-      
+
+
 def merge_data(df):
 
-# We're always asking for json because it's the easiest to deal with
-morph_api_url = "https://api.morph.io/rdmaloney/odds_scraper/data.json"
 
-# Keep this key secret!
-morph_api_key = "/y3liZqj/BLyw2JBNNXW"
+        # We're always asking for json because it's the easiest to deal with
+        morph_api_url = "https://api.morph.io/rdmaloney/odds_scraper/data.json"
 
-r = requests.get(morph_api_url, params={
-  'key': morph_api_key,
-  'query': "select * from "data" 
-})
+        # Keep this key secret!
+        morph_api_key = "/y3liZqj/BLyw2JBNNXW"
 
-odds_db = pd.DataFrame.from_dict(j)
+        r = requests.get(morph_api_url, params={
+            'key': morph_api_key,
+            'query': "select * from "data"
+        })
 
-test = pd.merge(df, odds_db, left_on=["Fighter1"], right_on["Fighter1"])
-test2 = pd.merge(test, odds_db, left on ["Fighter2"], right_on ["Fighter2"]
- 
-final_df = test2[[Event,Fighter1,Fighter2,Fighter1_Odds,Fighter2_Odds]]   
-                 
-return final_df
-                  
+        odds_db = pd.DataFrame.from_dict(j)
 
+        test = pd.merge(df, odds_db, left_on=["Fighter1"], right_on=["Fighter1"])
+        test2 = pd.merge(test, odds_db, left_on=["Fighter2"], right_on=["Fighter2"]
+
+        final_df = test2[[Event, Fighter1, Fighter2, Fighter1_Odds, Fighter2_Odds]]
+
+        return final_df
 
 scrape_data()
 df = create_df()
