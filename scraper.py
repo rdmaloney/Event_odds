bbfab893085b4579536b6094e29726d69950c738
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-29 22:49:04 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -85,7 +85,7 @@ def merge_data(df):
 
         test = pd.merge(df, odds_db, left_on=["Fighter_1"], right_on=["Fighter1"])
         test2 = pd.merge(test, odds_db, left_on=["Fighter_2"], right_on=["Fighter2"])
-        test3= pd.concat([test2,odds_db])
+        test3= pd.concat([test2,odds_db], sort=True)
  
         
         final_df = test3[['Event', 'Fighter_1', 'Fighter_2', 'Fighter1_Odds', 'Fighter2_Odds']]
